4ebe064d967eddcdde86fdf4bfbbce8bedd60b7a
arthurfiorette <arthur.fiorette@gmail.com>
2023-01-23 18:56:55 -0300

    fix: constant config to prevent literal errors

---
+++ .changeset/eight-jobs-itch.md
@@ -0,0 +1,6 @@
+---
+'@kitajs/generator': patch
+'@kitajs/runtime': patch
+---
+
+constant config to prevent literal errors
--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,3 +1,6 @@
 export * from './parameters';
 export * from './context';
 export * from './types';
+
+import type { KitaConfig, KitaGenerator, KitaAST, KitaError } from '@kitajs/generator';
+export type { KitaConfig, KitaGenerator, KitaAST, KitaError };
