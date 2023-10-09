76d8a26f070b92c4e795f2061ac2779a67fe03b6
Arthur Fiorette <me@arthur.place>
2023-10-09 19:39:50 -0300

    tag

--- packages/generator/test/providers/routes/index.ts
+++ packages/generator/test/providers/routes/index.ts
@@ -1,11 +1,6 @@
 import { MyProvider } from '../providers/a';
 
-/**
- * Hello world API endpoint. This comment will be used as the swagger description.
- *
- * The name parameter is going to be runtime validated and extract from que query string. If the name is not provided,
- * the default value will be used.
- */
+/** Default desc */
 export function get(test: MyProvider) {
   return test;
 }
