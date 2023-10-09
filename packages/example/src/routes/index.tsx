e55ac43861c62c263f89cb667c003abdbd38a1b3
Arthur Fiorette <me@arthur.place>
2023-10-09 17:59:24 -0300

    fix(example): removed jsx examples to pass other tests

--- packages/example/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../components/layout';
-
-export function get() {
-  return (
-    <Layout>
-      <div style={{ backgroundColor: 'red' }}>Hello World!</div>
-    </Layout>
-  );
-}
