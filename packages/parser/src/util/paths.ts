67fdc4d84328366d12638cb17977610438355a42
Arthur Fiorette <me@arthur.place>
2023-10-09 17:52:21 -0300

    perf(parser): faster walkdir

--- packages/parser/src/util/paths.ts
+++ packages/parser/src/util/paths.ts
@@ -1,15 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-export function walk(dir: string) {
+/** Synchronously walks the given directory and returns all files in it. */
+export function walk(dir: string): string[] {
   try {
-    return (
-      fs
-        .readdirSync(dir, { recursive: true, encoding: 'utf-8' })
-        .map((f) => path.join(dir, f))
-        // TODO: This may be slow, we should probably use a faster method
-        .filter((f) => fs.statSync(f).isFile())
-    );
+    return fs
+      .readdirSync(dir, { recursive: true, withFileTypes: true })
+      .flatMap((f) => (f.isDirectory() ? walk(path.join(dir, f.name)) : path.join(dir, f.name)));
   } catch (e: any) {
     // Ignore if folder does not exist
     if (e.code === 'ENOENT') {
