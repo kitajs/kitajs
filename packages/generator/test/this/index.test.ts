80f56e51405419d1afed7a792b05ea1c721ff323
Arthur Fiorette <me@arthur.place>
2023-10-11 11:40:02 -0300

    tests: added Use test

---
+++ packages/generator/test/this/index.test.ts
@@ -0,0 +1,63 @@
+import assert from 'node:assert';
+import test, { describe } from 'node:test';
+import { createApp, generateRuntime } from '../runner';
+
+//@ts-ignore - first test may not have been run yet
+import type Runtime from './runtime';
+
+describe('This & Use usage', async () => {
+  const rt = await generateRuntime<typeof Runtime>(__dirname);
+
+  test('expects getIndex was generated', () => {
+    assert.ok(rt);
+    assert.ok(rt.getIndex);
+    assert.ok(rt.getIndexHandler);
+    assert.ok(rt.postIndex);
+    assert.ok(rt.postIndexHandler);
+  });
+
+  test('GET / only has handler', () => {
+    //@ts-expect-error - internal property
+    const options = rt.getIndexOptions;
+
+    assert.ok(options.handler1 === undefined);
+    assert.ok(options.handler2);
+    assert.ok(options.handler3 === undefined);
+  });
+  test('POST / has 3 handler', () => {
+    //@ts-expect-error - internal property
+    const options = rt.postIndexOptions;
+
+    assert.ok(options.handler1);
+    assert.ok(options.handler2);
+    assert.ok(options.handler3);
+  });
+
+  test('getIndex registers handler 2', async () => {
+    const app = createApp(rt);
+
+    const res = await app.inject({ method: 'GET', url: '/' });
+
+    assert.equal(res.statusCode, 200);
+    assert.equal(res.body, 'Hello World!');
+    assert.equal(res.headers['x-handler1'], undefined);
+    assert.equal(res.headers['x-handler2'], 'true');
+    assert.equal(res.headers['x-handler3'], undefined);
+
+    await app.close();
+  });
+
+  test('postIndex registers 3 handlers', async () => {
+    const app = createApp(rt);
+
+    const res = await app.inject({ method: 'POST', url: '/' });
+
+    assert.equal(res.statusCode, 200);
+    assert.equal(res.body, 'Hello World!');
+    assert.equal(res.headers['x-handler1'], 'true');
+    assert.equal(res.headers['x-handler2'], 'true');
+    assert.equal(res.headers['x-handler3'], 'true');
+
+    await app.close();
+  });
+});
---
+++ packages/generator/test/this/routes/index.ts
@@ -0,0 +1,67 @@
+import type { RouteMapper, Use } from '@kitajs/runtime';
+import { RouteShorthandOptions } from 'fastify';
+
+export function get(this: Use<typeof handler2>) {
+  return 'Hello World!';
+}
+
+export function post(this: Use<[typeof handler1, typeof handler2, typeof handler3]>) {
+  return 'Hello World!';
+}
+
+export let handler1: RouteMapper = function handler1(config) {
+  if (!Array.isArray(config.preHandler)) {
+    if (config.preHandler) {
+      config.preHandler = [config.preHandler];
+    } else {
+      config.preHandler = [];
+    }
+  }
+
+  //@ts-expect-error - internal property
+  config.handler1 = true;
+  config.preHandler.push(function (_, res, next) {
+    res.header('x-handler1', 'true');
+    return next();
+  });
+
+  return config;
+};
+
+export const handler2: RouteMapper = function handler2(config) {
+  if (!Array.isArray(config.preHandler)) {
+    if (config.preHandler) {
+      config.preHandler = [config.preHandler];
+    } else {
+      config.preHandler = [];
+    }
+  }
+
+  //@ts-expect-error - internal property
+  config.handler2 = true;
+  config.preHandler.push(function (_, res, next) {
+    res.header('x-handler2', 'true');
+    return next();
+  });
+
+  return config;
+};
+
+export function handler3(config: RouteShorthandOptions) {
+  if (!Array.isArray(config.preHandler)) {
+    if (config.preHandler) {
+      config.preHandler = [config.preHandler];
+    } else {
+      config.preHandler = [];
+    }
+  }
+
+  //@ts-expect-error - internal property
+  config.handler3 = true;
+  config.preHandler.push(function (_, res, next) {
+    res.header('x-handler3', 'true');
+    return next();
+  });
+
+  return config;
+}
