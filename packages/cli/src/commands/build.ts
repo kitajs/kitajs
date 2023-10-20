d35639347ad5365f00cce4d342b61d565b3a4fbb
Arthur Fiorette <me@arthur.place>
2023-10-20 20:14:35 -0300

    file

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -88,7 +88,7 @@ export default class Build extends Command {
           cfg = cfg;
         }
 
-        ux.action.stop(chalk.cyan`Using found config!`);
+        ux.action.stop(chalk.cyan(`.${path.sep}${path.relative(root, flags.config)}`));
       }
 
       ux.action.start('Warming up', '', {
