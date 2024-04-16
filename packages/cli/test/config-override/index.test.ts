a9bcef74b0f37af91090cf163ab48dba9ade89fd
Arthur Fiorette <me@arthur.place>
2024-04-16 10:06:55 -0300

    fix: use forkSync

--- packages/cli/test/config-override/index.test.ts
+++ packages/cli/test/config-override/index.test.ts
@@ -1,25 +1,18 @@
 import assert from 'node:assert';
-import { spawnSync } from 'node:child_process';
 import path from 'node:path';
 import { it } from 'node:test';
-import { KITA_BIN } from '../constants';
+import { forkAsync } from '../constants';
 
-it('Prints overridden config', () => {
-  const cmd = spawnSync(KITA_BIN, ['config'], {
+it('Prints overridden config', async () => {
+  const cmd = await forkAsync(['config'], {
     cwd: __dirname,
     stdio: 'pipe',
-    encoding: 'utf-8',
     env: {
       ...process.env,
       KITA_RUNTIME_PATH: __dirname
     }
   });
 
-  if (cmd.status !== 0) {
-    console.debug(cmd);
-  }
-
-  assert.equal(cmd.status, 0);
   assert.equal(cmd.stderr, '');
 
   const cfg = JSON.parse(cmd.stdout);
--- packages/cli/test/help/index.test.ts
+++ packages/cli/test/help/index.test.ts
@@ -1,21 +1,16 @@
 import assert from 'node:assert';
-import { spawnSync } from 'node:child_process';
 import { it } from 'node:test';
-import { KITA_BIN } from '../constants';
+import { forkAsync } from '../constants';
 
-it('Prints Help Text', () => {
-  const cmd = spawnSync(KITA_BIN, ['--help'], {
+it('Prints Help Text', async () => {
+  const cmd = await forkAsync(['--help'], {
     cwd: __dirname,
     env: process.env,
-    encoding: 'utf-8'
+    stdio: 'pipe'
   });
 
-  if (cmd.status !== 0) {
-    console.debug(cmd);
-  }
-
   assert.equal(cmd.stderr, '');
-  assert.equal(cmd.status, 0);
+
   assert.ok(
     cmd.stdout.startsWith(
       'Performant and type safe fastify router - Build fast end-to-end APIs with ZERO abstraction cost!\n'
