413ce573b014345fb89d4bd8ad79f46a9d60f671
Arthur Fiorette <me@arthur.place>
2024-04-15 23:48:06 -0300

    code

--- packages/cli/test/constants.ts
+++ packages/cli/test/constants.ts
@@ -1,3 +1,3 @@
 import path from 'node:path';
 
-export const KITA_BIN = path.resolve(__dirname, '..', 'bin', 'dev.js');
+export const KITA_BIN = path.posix.resolve(__dirname, '..', 'bin', 'dev.js');
