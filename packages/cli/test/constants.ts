cde590afd6d972781af8e4267b653a99ab972966
Arthur Fiorette <me@arthur.place>
2024-04-15 23:38:53 -0300

    attempt 1

--- packages/cli/test/constants.ts
+++ packages/cli/test/constants.ts
@@ -1,9 +1,3 @@
 import path from 'node:path';
 
-export const KITA_BIN = path.resolve(
-  __dirname,
-  '..',
-  'bin',
-  // win32 uses a .cmd file
-  process.platform === 'win32' ? 'dev.cmd' : 'dev'
-);
+export const KITA_BIN = path.resolve(__dirname, '..', 'bin', 'dev.js');
