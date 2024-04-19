7e54c07e7fbe281ac15d3baab0c4bb8af91c5836
Arthur Fiorette <me@arthur.place>
2024-04-18 22:46:09 -0300

    feat: better create kita template

--- packages/cli/templates/kita/src/routes/index.ts
+++ packages/cli/templates/kita/src/routes/index.ts
@@ -1,13 +1,43 @@
-import type { Query } from '@kitajs/runtime';
+import type { Body, Query } from '@kitajs/runtime';
+import type { FastifyRequest } from 'fastify';
+
+/** A dto for creating a user */
+interface CreateUser {
+  /**
+   * @minLength 5
+   * @maxLength 20
+   */
+  name: string;
+
+  /** @format email */
+  email: string;
+}
 
 /**
+ * Simply returns a hello message
+ *
  * @tag Hello
  * @operationId getHello
- * @summary Get a hello message with date
+ * @summary Get a hello message
  */
 export function get(name: Query = 'World') {
   return {
     name,
     message: `Hello ${name}!`
+  };
+}
+
+/**
+ * Creates a user and logs it
+ *
+ * @tag User
+ * @operationId createUser
+ * @summary Create a user
+ */
+export function post(data: Body<CreateUser>, { log }: FastifyRequest) {
+  log.info('User created', data);
+
+  return {
+    created: 'User created'
   };
 }
