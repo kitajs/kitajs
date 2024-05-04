ab915f35a67019303570298157c06f3e32e5e877
Arthur Fiorette <me@arthur.place>
2024-05-04 18:15:52 -0300

    fix: cli tests

--- packages/cli/test/config-empty/index.test.ts
+++ packages/cli/test/config-empty/index.test.ts
@@ -1,9 +1,10 @@
 import assert from 'node:assert';
+import path from 'node:path';
 import { it } from 'node:test';
 import { forkAsync } from '../constants';
 
 it('Prints default config', async () => {
-  const cmd = await forkAsync(['config'], {
+  const cmd = await forkAsync(['config', '-r'], {
     cwd: __dirname,
     stdio: 'pipe',
     env: {
@@ -17,5 +18,5 @@ it('Prints default config', async () => {
 
   // No need to test all properties
   assert.equal(cfg.cwd, __dirname);
-  assert.equal(cfg.runtimePath, __dirname);
+  assert.equal(cfg.output, path.join(__dirname, 'src', 'runtime.kita.ts'));
 });
